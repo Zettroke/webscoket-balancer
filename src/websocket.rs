78eb24ccffe9b577588ac9362452537adbdf400f
Zettroke <skorostnoy2000@yandex.ru>
2020-03-13 02:54:12 +0300

    Cool error types

--- src/websocket.rs
+++ src/websocket.rs
@@ -1,6 +1,6 @@
-use std::net::{SocketAddr, ToSocketAddrs, SocketAddrV4, Ipv4Addr, Shutdown};
+extern crate memchr;
+use std::net::{SocketAddr, ToSocketAddrs, SocketAddrV4, Ipv4Addr};
 use tokio::net::{TcpListener, TcpStream};
-use std::error::Error;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use std::sync::atomic::{AtomicU64};
 use std::sync::Arc;
@@ -8,21 +8,36 @@ use std::collections::HashMap;
 use memchr::memchr;
 use crypto::sha1::Sha1;
 use crypto::digest::Digest;
-use httparse::Header;
+use thiserror::Error;
+// use core::slice::memchr::memchr;
 
+#[derive(Error, Debug)]
 enum HandshakeError {
-    TooManyHeaders
+    #[error("Bad request")]
+    BadRequest,
+    #[error("Headers parsing error: {0:?}")]
+    HeadersParsing(#[from] httparse::Error),
+    #[error("Handshake request was canceled")]
+    SocketClosed,
+    #[error("Missed header \"{0}\" in handshake request")]
+    MissedHeader(String),
+    #[error("Header \"{0}\" has bad value \"{1}\"")]
+    BadHeaderValue(String, String),
+    #[error("IO error happened: {0:?}")]
+    IOError(#[from] std::io::Error),
 }
+
 
 #[derive(Default, Debug)]
 struct WebsocketData {
     path: String,
+    /// Header name(key) is lowercase
     headers: HashMap<String, String>,
     query_params: HashMap<String, String>
 }
 
 struct WebsocketServerState {
-    received: AtomicU64
+    _received: AtomicU64
 }
 
 pub struct WebsocketServer {
@@ -37,7 +52,7 @@ impl WebsocketServer {
     pub fn new() -> Self {
         WebsocketServer {
             addr: SocketAddr::V4(SocketAddrV4::new(Ipv4Addr::new(127, 0, 0, 1), 8080)),
-            state: Arc::new(WebsocketServerState {received: AtomicU64::new(0)})
+            state: Arc::new(WebsocketServerState { _received: AtomicU64::new(0)})
         }
     }
 
@@ -46,69 +61,66 @@ impl WebsocketServer {
         self
     }
 
-
-    pub async fn run(&mut self) -> Result<(), Box<dyn Error>> {
-
-        let mut l = TcpListener::bind(self.addr).await?;
+    pub async fn run(&mut self) {
+        let mut l = TcpListener::bind(self.addr).await.unwrap();
         loop {
-            let (mut sock, _addr) = l.accept().await?;
+            let (sock, _addr) = l.accept().await.unwrap();
             tokio::spawn(Self::handler(self.state.clone(), sock));
         }
     }
 
-
-    async fn handler(this: Arc<WebsocketServerState>, mut socket: TcpStream) {
-
+    async fn handler(_this: Arc<WebsocketServerState>, mut socket: TcpStream) {
+        WebsocketServer::handshake(&mut socket).await.unwrap();
     }
 
-    async fn handshake(&mut socket: TcpStream) -> Result<> {
-        let data = match Self::receive_handshake_data(socket).await {
-            Ok(d) => d,
-            Err(_) => {
-                socket.shutdown(Shutdown::Both).unwrap();
-                return;
-            }
-        };
+    async fn handshake(socket: &mut TcpStream) -> Result<(), HandshakeError> {
+        let data = Self::receive_handshake_data(socket).await?;
 
-        println!("{:#?}", data);
-        if !data.headers.get("connection").map_or(false, |v| v.to_lowercase() == "upgrade") {
-            return;
+        // Check connection header
+        let conn = data.headers.get("connection").ok_or(HandshakeError::MissedHeader("connection".to_owned()))?;
+        if conn.to_lowercase() != "upgrade" {
+            return Err(HandshakeError::BadHeaderValue("connection".to_owned(), conn.to_owned()))
         }
-        if !data.headers.get("upgrade").map_or(false, |v| v.to_lowercase() == "websocket") {
-            return;
+        // Check upgrade header
+        let upg = data.headers.get("upgrade").ok_or(HandshakeError::MissedHeader("upgrade".to_owned()))?;
+        if upg.to_lowercase() != "websocket" {
+            return Err(HandshakeError::BadHeaderValue("upgrade".to_owned(), upg.to_owned()))
         }
 
-        let resp_key = if let Some(key) = data.headers.get("sec-websocket-key") {
-            let resp_key = key.to_owned() + "258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
-            let mut hasher = Sha1::new();
-            hasher.input_str(resp_key.as_str());
-            let mut output = [0u8; 20];
-            hasher.result(&mut output);
-            base64::encode(output)
-        } else {
-            return;
-        };
+        // Calculating Sec-WebSocket-Accept key
+        let key = data.headers.get("sec-websocket-key")
+            .ok_or(HandshakeError::MissedHeader("sec-websocket-key".to_owned()))?;
 
-        socket.write(format!("HTTP/1.1 101 Switching Protocols\r\nUpgrade: websocket\r\nConnection: Upgrade\r\nSec-WebSocket-Accept: {}\r\n\r\n", resp_key).as_bytes()).await.unwrap();
+        let mut hasher = Sha1::new();
+        hasher.input_str(key);
+        hasher.input_str("258EAFA5-E914-47DA-95CA-C5AB0DC85B11");
+        let mut output = [0u8; 20];
+        hasher.result(&mut output);
+        let resp_key = base64::encode(output);
+
+        socket.write(
+            format!("\
+            HTTP/1.1 101 Switching Protocols\r\n\
+            Upgrade: websocket\r\n\
+            Connection: Upgrade\r\n\
+            Sec-WebSocket-Accept: {}\r\n\
+            \r\n", resp_key).as_bytes()
+        ).await?;
 
         println!("Connected!!!!!1");
-        let mut buff = [0u8; 1024];
-        loop {
-            socket.read(&mut buff).await.unwrap();
-            println!("msg!");
-        }
+        return Ok(());
 
 
     }
 
-    async fn receive_handshake_data(socket: &mut TcpStream) -> Result<WebsocketData, ()> {
+    async fn receive_handshake_data(socket: &mut TcpStream) -> Result<WebsocketData, HandshakeError> {
         let mut handshake: Vec<u8> = Vec::with_capacity(2048);
         let mut buff = [0; 2048];
         let mut prev_packet_ind: usize = 0;
 
         // handshake
         loop {
-            let n = socket.read(&mut buff).await.unwrap();
+            let n = socket.read(&mut buff).await?;
             handshake.extend_from_slice(&buff[0..n]);
             if handshake[n-2..n] == *"\r\n".as_bytes() {
                 break;
@@ -119,22 +131,17 @@ impl WebsocketServer {
             }
             prev_packet_ind += n;
             if n == 0 {
-                return Err(());
+                return Err(HandshakeError::SocketClosed);
             }
         }
         let mut headers = [httparse::EMPTY_HEADER; 30];
 
         let mut req = httparse::Request::new(&mut headers);
-        match req.parse(handshake.as_slice()) {
-            Ok(httparse::Status::Complete(_len)) => {},
-            _ => {
-                return Err(());
-            }
-        }
+        req.parse(handshake.as_slice())?;
 
         let mut res = WebsocketData::default();
         // TODO: URLdecode
-        let path = req.path.unwrap();
+        let path = req.path.ok_or(HandshakeError::BadRequest)?;
         let q_index = memchr(b'?', path.as_bytes()).unwrap_or(path.len());
         res.path = path[0..q_index].to_string();
 
@@ -163,14 +170,10 @@ impl WebsocketServer {
             if *h == httparse::EMPTY_HEADER {
                 break;
             }
-            match String::from_utf8(h.value.to_vec()) {
-                Ok(v) => res.headers.insert(h.name.to_lowercase(), v),
-                Err(_) => continue
-            };
+
+            let v = String::from_utf8(h.value.to_vec()).map_err(|_| HandshakeError::BadRequest)?;
+            res.headers.insert(h.name.to_lowercase(), v);
         }
-
-
-        // socket.write(format!("HTTP/1.1 200\r\n\r\n<pre>{:#?}</pre>\r\n", res).as_bytes()).await;
 
         Ok(res)
     }
